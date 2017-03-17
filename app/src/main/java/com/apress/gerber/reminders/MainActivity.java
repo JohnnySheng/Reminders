b582cd29dae2d0fdd7dfe212e1ac77439ab35d10
Yuangang Sheng <yuangangsheng@YuangangdeMacBook.local>
2017-03-17 10:08:48 +0800

    add darkgrey to colors.xml

--- app/src/main/java/com/apress/gerber/reminders/MainActivity.java
+++ app/src/main/java/com/apress/gerber/reminders/MainActivity.java
@@ -2,8 +2,11 @@ package com.apress.gerber.reminders;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +14,10 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        mListView = (ListView) findViewById(R.id.reminders_list_view);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.reminders_row, R.id.row_textview, new String[]{"first record", "second record", "third record"});
+
+        mListView.setAdapter(arrayAdapter);
     }
 }
